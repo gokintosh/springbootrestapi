fed37409e5616b7f1f8797def21283702731e463
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-11-11 13:18:13 +0100

    initial commit

---
+++ src/main/java/com/gokul/springbrainutub/springbrainutub/SpringbrainutubApplication.java
@@ -0,0 +1,13 @@
+package com.gokul.springbrainutub.springbrainutub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbrainutubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbrainutubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gokul/springbrainutub/springbrainutub/SpringbrainutubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gokul.springbrainutub.springbrainutub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbrainutubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
